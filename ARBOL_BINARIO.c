f7a188cf33a1a99d5bc6b31a2a01413b1d452b3e
Hoffman99 <zS24013023@estudiantes.uv.mx>
2025-02-18 14:23:50 -0600

    Update ARBOL_BINARIO.c

--- ARBOL_BINARIO.c
+++ ARBOL_BINARIO.c
@@ -13,6 +13,7 @@ typedef struct{
 void buscar(binario arbol[bin], int *indice);
 void insertar_valor(binario arbol[bin], int *indice);
 int nivel (binario arbol[bin], int indice);
+void mostrar(binario arbol[bin]);
 
 int main(){
     binario arbol[bin];
@@ -22,19 +23,23 @@ int main(){
         printf("\n1. Insertar un valor\n");
         printf("2. Buscar un valor\n");
         printf("3. Recorrido en orden\n");
-        printf("4. Cerrar programa\n");
+        printf("4. Mostrar\n")
+        printf("5. Cerrar programa\n");
     scanf("%d", &eleccion);
         switch(eleccion){
             case 1:
                 insertar_valor(arbol, &indice);
             break;
             case 2:
-                buscar();
+                buscar(arbol, &indice);
             break;
             case 3:
-                recorrido();
+             //   recorrido();
             break;
             case 4:
+                mostrar(arbol);
+            break;
+            case 5:
                 band=1;
             break;
             default:
@@ -45,7 +50,7 @@ int main(){
 }
 
 int nivel (binario arbol[bin], int i){
-    int nivel_anterior=arbol[i-1].nivel, resultado=1, nivel=0;
+    int nivel_anterior=arbol[i-1].nivel, resultado=1, nivel=1;
     for (int i = 0; i < nivel_anterior; i++) {
         resultado *= 2;
     }
@@ -58,19 +63,19 @@ int nivel (binario arbol[bin], int i){
 }
 
 int padre(binario arbol[bin], int i){
-`   int nodo=arbol[*indice].arbol, padre;
-    padre=(int)nodo/2;
+   int trans=arbol[i].arbol, padre;
+    padre=(int)trans/2;
     return padre;
 }
 
 void insertar_valor(binario arbol[bin], int *indice){
     int respuesta, i=*indice;
-    printf("¿Cuál número deseas agregar al árbol?/n");
+    printf("¿Cuál número deseas agregar al árbol?\n");
     scanf("%d", &respuesta);
     if(*indice==0){
         arbol[*indice].arbol=i+1;
-        arbol[*indice].izquierda=respuesta
-        arbol[*indice].nivel=0;
+        arbol[*indice].izquierda=respuesta;
+        arbol[*indice].nivel=1;
     }else{
         if(*indice%2!=0){
             arbol[*indice].izquierda=respuesta;
@@ -80,9 +85,8 @@ void insertar_valor(binario arbol[bin], int *indice){
         arbol[*indice].arbol=i+1;
         arbol[*indice].nivel=nivel(arbol, i);
         arbol[*indice].padre=padre(arbol, i);
-        (*indice)++;
     }
-
+    (*indice)++;
 }
 
 void buscar(binario arbol[bin], int *indice){
@@ -140,3 +144,18 @@ void buscar(binario arbol[bin], int *indice){
         printf("\nEl valor no se encontró dentro del árbol\n");
     }
 }  
+
+void mostrar(binario arbol[bin]){
+    for(int i=0;i<9;i++){
+        int lado=arbol[i].arbol;
+        if (lado==1){
+            printf("#%d en el nodo %d, su padre es %d y esta en el nivel %d\n", arbol[i].izquierda, arbol[i].arbol, arbol[i].padre, arbol[i].nivel);
+        }else{
+            if(i%2==0){
+                printf("#%d en el nodo %d, su padre es %d y esta en el nivel %d\n", arbol[i].derecha, arbol[i].arbol, arbol[i].padre, arbol[i].nivel);
+            }else{
+                printf("#%d en el nodo %d, su padre es %d y esta en el nivel %d\n", arbol[i].izquierda, arbol[i].arbol, arbol[i].padre, arbol[i].nivel);
+            }
+        }
+    }
+} 
