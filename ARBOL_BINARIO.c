7c5c2a12c7ba9d4f7ee396a1a89d6a94e14cfff5
western1258 <122416882+western1258@users.noreply.github.com>
2025-02-19 21:57:07 -0600

    Update ARBOL_BINARIO.c

--- ARBOL_BINARIO.c
+++ ARBOL_BINARIO.c
@@ -140,15 +140,15 @@ void transver(binario arbol[bin], int posicion){
     int trans; 
     while (posicion>=0){
         trans = arbol[posicion].padre;
-        if((trans%2)==0){
-            printf("#%d en el nodo %d, su padre es %d y esta en el nivel %d\n", arbol[posicion].derecha, arbol[posicion].arbol, arbol[posicion].padre, arbol[posicion].nivel);
+        if(trans==0){
+      printf("#%d en el nodo %d, y es el nodo raiz %d\n", arbol[posicion].izquierda, arbol[posicion].arbol);
         }else if((trans%2)!=0){
             printf("#%d en el nodo %d, su padre es %d y esta en el nivel %d\n", arbol[posicion].izquierda, arbol[posicion].arbol, arbol[posicion].padre, arbol[posicion].nivel);
         }else{
-            printf("#%d en el nodo %d, y es el nodo raiz %d\n", arbol[posicion].izquierda, arbol[posicion].arbol);
-            
+            printf("#%d en el nodo %d, su padre es %d y esta en el nivel %d\n", arbol[posicion].derecha, arbol[posicion].arbol, arbol[posicion].padre, arbol[posicion].nivel);
+ 
         }
         posicion = trans - 1;    /* code */
 
     }
-    }
+    }            
