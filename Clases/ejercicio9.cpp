caade93c6a0dc43b7d46bbc8de14fde74bcbd6bc
Hoffman99 <zS24013023@estudiantes.uv.mx>
2025-03-24 20:32:20 -0600

    Update ejercicio9.cpp

--- Clases/ejercicio9.cpp
+++ Clases/ejercicio9.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-class Polinomio{
+class Polinomio{//Crea una clase de polinomios ax²+bx+c
     private:
         int a;
         int b;
         int c;
     public:
-        friend void crear(Polinomio& p1, Polinomio& p2){
+        friend void crear(Polinomio& p1, Polinomio& p2){//Contructor para crear los 2 polinomios respecto a la información proporcionada por el usuario
             cout<<"Crear 2 Polinomios"<<endl;
             cout<<"Considerando que ax²+bx+c, ingrese a, b, c."<<endl;
             cout<<"Primer Polinomio:"<<endl;
@@ -26,14 +26,14 @@ class Polinomio{
             cout<<"c ==> ";
             cin>>p2.c;
         }
-        friend void evaluar(Polinomio& p){
+        friend void evaluar(Polinomio& p){//Constructo para evaluar el Polinomio en un punto proporcionado por el usuario, haciendo las cuentas necesarias para obtener el resultado
             double x=0, total;
             cout<<"Ingrese el punto en el que desea evaluar el polinomio ==> ";
             cin>>x;
             total=((x*x)*p.a)+(x*p.b)+p.c;
             cout<<"El resultado es ==> "<<total<<endl;
         }
-        void sumar(Polinomio& p1, Polinomio& p2){
+        void sumar(Polinomio& p1, Polinomio& p2){////Constructor para sumar los 2 polinomios y mostrar el resultado
             a=p1.a+p2.a;
             b=p1.b+p2.b;
             c=p1.c+p2.c;
@@ -43,7 +43,7 @@ class Polinomio{
 };
 
 int main(){
-    Polinomio polinomio1, polinomio2, polinomioTotal;
+    Polinomio polinomio1, polinomio2, polinomioTotal;//Para el programa el usuario solo va interactuar con el polinomio 1 y 2, el Total, va a servir para realizar la suma de estos.
     int band=1;
     crear(polinomio1, polinomio2);
     while(band==1){
